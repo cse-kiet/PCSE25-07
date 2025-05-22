b1179407aa2e71446982fdf521cc9b380e2ac38d
Abhishek <abhishekofficial9453@gmail.com>
2025-05-23 00:07:07 +0530

    Initial commit

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,20 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+# Load the model
+model = load_model('models/2.keras')
+
+# Load and preprocess the image
+img_path = 'E:/Skin/fuc.jpeg'  # Corrected path with forward slashes
+img = image.load_img(img_path, target_size=(224, 224))  # Assuming the target size expected by your model
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
+# Predict
+predictions = model.predict(img_array)
+print(predictions)
---
+++ api/tempCodeRunnerFile.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+from io import BytesIO
+from PIL import Image 
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model     
+import tensorflow.keras as keras
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI ()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+#MODEL = tf.keras.models.load_model('E:\Potato-Project\savedmodel\1.keras')
+# MODEL = load_model('models/1.keras')
+# model = load_model('E:/Skin/models/xyz.keras') 
+MODEL = load_model('E:/Skin/api/models/2.keras')
+
+# print(model.summary())
+CLASS_NAMES = ['Acne and Rosacea Photos',
+ 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
+ 'Atopic Dermatitis Photos',
+ 'Eczema Photos',
+ 'Exanthems and Drug Eruptions',
+ 'Melanoma Skin Cancer Nevi and Moles',
+ 'Tinea Ringworm Candidiasis and other Fungal Infections']
+@app.get("/ping")
+async def ping():
+ return "Hello, I am alive"
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)): 
+   image = read_file_as_image(await file.read()) 
+   image_batch = np.expand_dims(image,0)
+   predictions = MODEL.predict(image_batch) 
+   predicted_class = CLASS_NAMES[np.argmax(predictions)]
+   confidence=np.max(predictions[0])
+   print(predicted_class, confidence)
+   return {
+      "class": predicted_class,
+     "confidence":float(confidence)
+   }
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
